dc5fb49fc9d9a084edf78c04e812b07b42b948f3
Matheus Gonçalves Oliveira <matheusgoncalves56@gmail.com>
2016-08-07 11:59:29 -0300

    Commit inicial

---
+++ loja/src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -0,0 +1,52 @@
+package br.com.alura.loja.modelo;
+
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+
+public class Projeto {
+	private String nome;
+	private Long id;
+	private int anoDeInicio;
+	
+	public Projeto() {
+	
+	}
+
+
+	public Projeto(Long id, String nome, int anoDeInicio) {
+		super();
+		this.nome = nome;
+		this.id = id;
+		this.anoDeInicio = anoDeInicio;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public int getAnoDeInicio() {
+		return anoDeInicio;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String toXML() {
+		return new XStream().toXML(this);
+	}
+
+
+	public String toJson() {
+		return new Gson().toJson(this);
+	}
+	
+}
---
+++ loja/src/test/java/br/com/alura/loja/CarrinhoTest.java
@@ -0,0 +1,63 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
+import junit.framework.Assert;
+
+public class CarrinhoTest {
+
+	private HttpServer server;
+
+	@Before
+	public void before() {
+		this.server = Servidor.startaServidor();
+	}
+
+	@After
+	public void mataServidor() {
+		server.stop();
+	}
+
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080/");
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
+
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+
+	}
+	
+	public void testaPostCarrinhoEsperado(){
+		Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+       
+        Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        
+        String xml = carrinho.toXML();
+        
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+        Response response = target.path("/carrinhos").request().post(entity);
+        Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
+		
+	}
+}
